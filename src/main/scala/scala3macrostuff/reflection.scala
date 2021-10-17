a55246ddb429e314ce68cb045f3e53762ce3c709
Carlos Rueda <carueda@mbari.org>
2021-10-17 14:29:54 -0700

    some matches

--- src/main/scala/scala3macrostuff/reflection.scala
+++ src/main/scala/scala3macrostuff/reflection.scala
@@ -11,8 +11,24 @@ inline def refl(inline n: Int) = ${ reflImpl('n) }
 
 private def reflImpl(x: Expr[Int])(using Quotes): Expr[Int] =
   import quotes.reflect.*
+  given Printer[Tree] = Printer.TreeStructure
+
   val tree: Term = x.asTerm
   scribe.warn(s"tree = $tree")
+  scribe.warn(s"tree = ${tree.show}")
+
+  // just playing with matches ...
+  tree match
+    // Inlined(call: Option[Tree /* Term | TypeTree */], bindings: List[Definition], expansion: Term)
+    case Inlined(None, Nil, Literal(IntConstant(c))) =>
+      // this of course must be our case here.
+      scribe.warn(s"IntConstant=$c")
+
+    case Literal(_)   =>
+    case Select(_, _) =>
+    case _            => throw new MatchError(tree.show)
+
   val expr: Expr[Int] = tree.asExprOf[Int]
   scribe.warn(s"expr = $expr")
+  scribe.warn(s"expr = ${expr.show}")
   expr
